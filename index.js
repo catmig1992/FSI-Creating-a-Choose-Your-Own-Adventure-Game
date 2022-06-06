b61807a8eedb107c2dbffa53d1c1bd26e2a7a09a
Catherine Miguel <catmig1992@gmail.com>
2022-06-06 10:28:34 -0700

    specified text box options for users

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (firstAnswer === "left") {
   );
   if (secondAnswer === "follow") {
     let thirdAnswer = window.prompt(
-      `You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`
+      `You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven. Do you stay or spread the word?`
     );
     if (thirdAnswer === "stay") {
       window.alert(
@@ -20,7 +20,7 @@ if (firstAnswer === "left") {
   }
   if (secondAnswer === "continue") {
     let thirdAnswer = window.prompt(
-      `You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`
+      `You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take, the ladder or staircase?`
     );
     if (thirdAnswer === "ladder") {
       window.alert(
@@ -35,7 +35,7 @@ if (firstAnswer === "left") {
   }
 } else if (firstAnswer === "right") {
   let secondAnswer = window.prompt(
-    `You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?`
+    `You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take, past the dragon or away from the dragon?`
   );
   if (secondAnswer === "past the dragon") {
     let thirdAnswer = window.prompt(
@@ -54,7 +54,7 @@ if (firstAnswer === "left") {
   }
   if (secondAnswer === "away from the dragon") {
     let thirdAnswer = window.prompt(
-      `After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?`
+      `After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do, draw it or pick it?`
     );
     if (thirdAnswer === "draw it") {
       window.alert(
